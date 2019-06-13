7479faa1f7d3f74e03df6a02940695630779b515
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-06-13 15:38:01 +0800

    Add quick form for member

--- views/components/acs/Member.vue
+++ views/components/acs/Member.vue
@@ -9,9 +9,11 @@
             }
             " -->
 
-        <iv-table
+        <!-- <iv-table
             :server="{ path: '/acs/member' }"
-            />
+            /> -->
+
+        <MemberForm />
     </div>
 </template>
 
@@ -20,8 +22,11 @@
 import { Component, Vue } from 'vue-property-decorator';
 import { RegisterRouter } from '@/../core/router';
 import { toEnumInterface } from '@/../core';
+import MemberForm from './MemberForm.vue';
 
-@Component
+@Component({
+    components: { MemberForm }
+})
 export default class Member extends Vue {
     private isMounted: boolean = false;
     private doMounted() {
