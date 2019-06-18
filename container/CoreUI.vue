d56e79cf8d36831e6422b30df1b847be5af34824
benlee0918 <bingxain@hotmail.com>
2019-06-18 15:01:14 +0800

    fix Version conflict

--- container/CoreUI.vue
+++ container/CoreUI.vue
@@ -87,6 +87,8 @@
                     <SidebarNavItem
                         :label="_('w_Navigation_VideoSources_HumanDetection')"
                         url="/video_sources/human_detection"
+                    />
+                    <SidebarNavItem
                         :label="_('w_Navigation_VideoSources_DwellTime')"
                         url="/video_sources/dwell_time"
                     />
