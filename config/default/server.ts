dabafcd824f735230868afde7df43e4eb39ad150
Tina.Lin <7070587@gmail.com>
2019-06-20 19:57:34 +0800

    update server api

--- config/default/server.ts
+++ config/default/server.ts
@@ -1,61 +1,77 @@
 import { iSAPServerBase, ApisRequestBase, InputR, OutputR } from '@/../core/server';
 
-interface RestfulRequest extends ApisRequestBase { 
-    "Get": { 
+interface RestfulRequest extends ApisRequestBase {
+    "Post": {
+        "/users/login": [UsersLoginAll.Input, UsersLoginAll.Output, false],
+        "/users/logout": [UsersLogoutPost.Input, any, true],
+        "/users": [any, any, true]
+
+        // Member
+        "/acs/member": [any, any, true]
+    },
+
+    "Get": {
         "/acs/door": [any, any, true],
-        "/users/login": [UsersLoginAll.Input, UsersLoginAll.Output, false],        
+        "/users/login": [UsersLoginAll.Input, UsersLoginAll.Output, false],
         "/users": [any, any, true],
         "/announcements": [AnnouncementsGet.Input, AnnouncementsGet.Output, true]
-    }, 
-    "Post": { 
-        "/users/login": [UsersLoginAll.Input, UsersLoginAll.Output, false], 
-        "/users/logout": [UsersLogoutPost.Input, any, true], 
-        "/users": [any, any, true]
+
+        // Member
+        "/acs/member": [any, any, true]
+        "/acs/workgroup": [any, any, true]
+
     },
+
     "Put": {
         "/users": [any, any, true]
+
+        // Member
+        "/acs/member": [any, any, true]
     },
     "Delete": {
         "/users": [any, any, true]
+
+        // Member
+        "/acs/member": [any, any, true]
     },
     "Ws": {
-        "/users/alive": [any, any, true], 
-        "/agents/connect": [any, any, true], 
-    }, 
+        "/users/alive": [any, any, true],
+        "/agents/connect": [any, any, true],
+    },
 }
 
 export class SmartCommunityServer extends iSAPServerBase<RestfulRequest> {}
 export default SmartCommunityServer;
 
-/// /users/login - All ///////////////////////////////////// 
-declare namespace UsersLoginAll { 
-    export interface Input { 
-        username: string; 
-        password: string; 
-        } 
+/// /users/login - All /////////////////////////////////////
+declare namespace UsersLoginAll {
+    export interface Input {
+        username: string;
+        password: string;
+        }
 
-        export interface Output { 
-        sessionId: string; 
-        serverTime: Date; 
-        user: string; 
-    } 
-} 
-////////////////////////////////////////////////////////////// 
-/// /users/logout - Post ///////////////////////////////////// 
-declare namespace UsersLogoutPost { 
-    export interface Input { 
-    sessionId: string; 
-    } 
-} 
-////////////////////////////////////////////////////////////// 
+        export interface Output {
+        sessionId: string;
+        serverTime: Date;
+        user: string;
+    }
+}
+//////////////////////////////////////////////////////////////
+/// /users/logout - Post /////////////////////////////////////
+declare namespace UsersLogoutPost {
+    export interface Input {
+    sessionId: string;
+    }
+}
+//////////////////////////////////////////////////////////////
 /// /announcements - Get /////////////////////////////////////
-export declare namespace AnnouncementsGet { 
+export declare namespace AnnouncementsGet {
     export interface IAnnouncements {
         /**
         * Title of announcements.
         */
         title: string;
-        
+
         /**
         * Content of announcements.
         */
@@ -64,14 +80,14 @@ export declare namespace AnnouncementsGet {
     export type Input = InputR<IAnnouncements>;
     export type Output = OutputR<IAnnouncements>;
 }
-////////////////////////////////////////////////////////////// 
-declare namespace Member{    
+//////////////////////////////////////////////////////////////
+declare namespace Member{
     export interface Input {
         memberid: string;
         firstname: string;
         middlename: string;
         lastname: string;
-        status: number; 
+        status: number;
         cardnNum : string;
     }
     export interface Output{
@@ -80,7 +96,7 @@ declare namespace Member{
         firstname: string;
         middlename: string;
         lastname: string;
-        status: number; 
+        status: number;
         cardnNum : string;
         createdAt:Date;
         updatedAt:Date;
