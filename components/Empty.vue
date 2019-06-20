92513bce2bf29212813a7f8d0df78afffd9ac446
Morris's Macbook Pro <Morris.Hsu@isapsolution.com>
2019-06-20 11:33:28 +0800

    components empty

---
+++ components/Empty.vue
@@ -0,0 +1,41 @@
+<template>
+    <div ref="mapParent">
+
+        <div>
+        </div>
+
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Emit } from "vue-property-decorator";
+
+@Component({
+    components: {}
+})
+export class Empty extends Vue {
+    @Prop({
+        type: String,
+        default: ""
+    })
+    label: string;
+
+    inputData = "Test input data";
+
+    created() {}
+
+    mounted() {
+        this.start();
+    }
+
+    start() {
+        this.$emit("input", this.inputData);
+    }
+}
+
+export default Empty;
+Vue.component("empty", Empty);
+</script>
+
+<style lang="scss" scoped>
+</style>
