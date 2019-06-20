af00636d81588d1a47bbe1b3fd7b4030f140b983
benlee0918 <bingxain@hotmail.com>
2019-06-20 21:05:58 +0800

    fix radio

--- views/components/acs/PermissionTable.vue
+++ views/components/acs/PermissionTable.vue
@@ -71,10 +71,8 @@
                         <div class="card-content iv-form-group col-md-12">
                             <b-form-group :label="_('w_Permission_DeviceType')">
                                 <b-form-radio-group
-                                    id="radio-group-1"
-                                    v-model="selected"
-                                    :options="options"
-                                    name="radio-options"
+                                    v-model="$attrs.value"
+                                    :options="devoceTypeItem"
                                 ></b-form-radio-group>
                             </b-form-group>
                         </div>
@@ -145,9 +143,9 @@ export default class PermissionTable extends Vue {
     deviceAreaItem: any = [];
     deviceTimeFromatItem: any = [];
     devoceTypeItem: any = [
-        { text: "Door", value: "first" },
-        { text: "Door Group", value: "second" },
-        { text: "Elevator", value: { fourth: 4 } }
+        { text: "Door", value: "door" },
+        { text: "Door Group", value: "doorGroup" },
+        { text: "Elevator", value: "elevator" }
     ];
 
     created() {}
