32295aed817d9f5a60ad2c44d6770fbc3996b152
Tina.Lin <7070587@gmail.com>
2019-06-20 19:07:46 +0800

    add menber form page

--- views/components/acs/Member.vue
+++ views/components/acs/Member.vue
@@ -1,6 +1,7 @@
 <template>
      <div key="main">
         <MemberForm />
+        <MemberForm1 />
     </div>
 </template>
 
@@ -10,9 +11,10 @@ import { Component, Vue } from 'vue-property-decorator';
 import { RegisterRouter } from '@/../core/router';
 import { toEnumInterface } from '@/../core';
 import MemberForm from './MemberForm.vue';
+import MemberForm1 from './MemberForm1.vue';
 
 @Component({
-    components: { MemberForm }
+    components: { MemberForm1 }
 })
 export default class Member extends Vue {
     private isMounted: boolean = false;
