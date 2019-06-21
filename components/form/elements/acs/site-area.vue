d0c211b3f42aac4e361dbd8c27433566d1bb8df6
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-06-21 22:05:23 +0800

    Add site area ivc-form

---
+++ components/form/elements/acs/site-area.vue
@@ -0,0 +1,48 @@
+<template>
+    <b-form-group
+        v-bind:id="'form-group-' + _uid"
+        v-bind:label="label"
+        v-bind:label-for="'input-' + _uid"
+        >
+         <b-input-group>    
+            <b-form-select
+                @change="updateArea()"
+                v-model="siteObjectId"
+            >
+                <option />
+                <option
+                    v-for="option in sites"  
+                    :key="option.key"
+                    :value="option.key"               
+                    >
+                    {{ option.value }}
+                </option>
+            </b-form-select> 
+
+                <b-form-select
+                @change="update()"
+                v-model="areaObjectId"
+            >
+                <option />
+                <option 
+                    v-for="option in areas"  
+                    :key="option.key"
+                    :value="option.key"               
+                    >
+                    {{ option.value }}
+                </option>
+            </b-form-select>
+
+         </b-input-group>
+        <b-form-invalid-feedback v-if="this.invalid">
+            {{ this.invalid }}
+        </b-form-invalid-feedback>
+    </b-form-group>
+</template>
+
+<script lang="ts" src="./site-area.vue.ts" />
+
+<style lang="scss">
+
+</style>
+
