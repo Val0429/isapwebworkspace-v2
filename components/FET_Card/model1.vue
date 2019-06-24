26cba94cf92cbd1380f8f588d0fa9b5018fee8b0
Morris's Macbook Pro <Morris.Hsu@isapsolution.com>
2019-06-24 18:32:03 +0800

    temp save about FET card

---
+++ components/FET_Card/model1.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="fet-card-printer">
+        <img
+            class="img-logo"
+            :src="logoSrc"
+        >
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Emit } from "vue-property-decorator";
+import IconBase64 from "./models/IconBase64";
+
+@Component({
+    components: {}
+})
+class FETCardModel1 extends Vue {
+    // Prop
+    // @Prop({
+    //     type: String,
+    //     default: ""
+    // })
+    // photo: string;
+
+    // @Prop({
+    //     type: String,
+    //     default: ""
+    // })
+    // chineseName: string;
+
+    // @Prop({
+    //     type: String,
+    //     default: ""
+    // })
+    // englishName: string;
+
+    // @Prop({
+    //     type: String,
+    //     default: ""
+    // })
+    // cardNumber: string;
+
+    logoSrc: string = "";
+
+    // Vue step
+    created() {
+        this.logoSrc = IconBase64.model1Logo;
+        console.log(this.logoSrc);
+    }
+
+    mounted() {}
+
+    printCard() {
+        window.print();
+    }
+}
+
+export default FETCardModel1;
+Vue.component("fet-card-model-1", FETCardModel1);
+</script>
+
+<style lang="scss" scoped>
+.fet-card-printer {
+    width: 100%;
+    .img-logo {
+        width: 50%;
+        margin-left: 25%;
+        margin-right: 25%;
+    }
+}
+
+@media print {
+    .fet-card-printer {
+        display: block;
+    }
+}
+
+@media screen {
+    // .fet-card-printer {
+    //     display: none;
+    // }
+}
+</style>
---
+++ views/Demos/DemoFETCardPrint.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="animated fadeIn">
+        <b-button @click="printCard">
+            Print
+        </b-button>
+        <fet-card-model-1
+            ref="fetCardModel1"
+            class="card-model"
+        >
+        </fet-card-model-1>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Emit } from "vue-property-decorator";
+import FETCardModel1 from "@/components/FET_Card/model1.vue";
+
+@Component({
+    components: {
+        FETCardModel1
+    }
+})
+export default class DemoFETCardPrint extends Vue {
+    created() {}
+    mounted() {}
+    printCard() {
+        let model: any = this.$refs.fetCardModel1;
+        model.printCard();
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
