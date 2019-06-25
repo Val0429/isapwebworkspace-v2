1527cac83306749a0c55d2e83ece1fb47518f25f
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-06-25 15:17:44 +0800

    Add system selection

---
+++ components/form/elements/acs/system-selection.vue
@@ -0,0 +1,36 @@
+<template>
+    <b-form-group
+        v-bind:id="'form-group-' + _uid"
+        v-bind:label="label"
+        v-bind:label-for="'input-' + _uid"
+        >
+             <b-row>
+                <b-col>  
+                <b-form-select
+                    @change="update()"
+                    v-model="system"
+                >
+                <option />
+                <option
+                    v-for="option in options"  
+                    :key="option.key"
+                    :value="option.key"               
+                    >
+                    {{ option.value }}
+                </option>
+                </b-form-select> 
+                </b-col>                
+                
+             </b-row>
+        <b-form-invalid-feedback v-if="this.invalid">
+            {{ this.invalid }}
+        </b-form-invalid-feedback>
+    </b-form-group>
+</template>
+
+<script lang="ts" src="./system-selection.vue.ts" />
+
+<style lang="scss">
+
+</style>
+
