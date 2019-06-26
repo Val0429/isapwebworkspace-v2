45b0b755979e7c32a0cc1d7fba128aba8bd396b0
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-06-26 14:02:33 +0800

    Refactor class and functions

--- views/components/acs/reports/DemographicReport.vue
+++ views/components/acs/reports/DemographicReport.vue
@@ -15,8 +15,9 @@
             
         
         <template v-if="isMounted">
-            <b-button size="lg" v-bind="$refs.form.submitBindings.$attrs" v-on="$refs.form.submitBindings.$listeners" >{{ _("wb_Submit") }}</b-button>
-            <b-button size="lg" v-bind="$refs.form.resetBindings.$attrs" v-on="$refs.form.resetBindings.$listeners" @click="onSubmit()">{{ _("wb_Reset") }}</b-button>
+            <b-button class="btn-filter" size="lg" v-bind="$refs.form.submitBindings.$attrs" v-on="$refs.form.submitBindings.$listeners" >{{ _("wb_Submit") }}</b-button>
+            <b-button class="btn-filter" size="lg" v-bind="$refs.form.resetBindings.$attrs" v-on="$refs.form.resetBindings.$listeners" @click="onSubmit()">{{ _("wb_Reset") }}</b-button>
+            <b-button class="btn-filter" size="lg" @click="exportToExcel()" >{{ _("wb_Export") }}</b-button>            
         </template>
         </iv-card>
          <iv-card            
@@ -34,25 +35,21 @@
 
         </iv-card>
         <iv-card            
-            :label="_('w_EmployeeList')">        
+            :label="tableTitle">        
         
            <b-table striped hover 
-                :items="employeeList" 
-                :fields="fields().filter(x=> selectedColumns.find(y=>y==x.key))" 
+                :items="records" 
+                :fields="fields.filter(x=> selectedColumns.find(y=>y==x.key))" 
                 :per-page="perPage"
                 :busy="isBusy"
                 :current-page="currentPage"
            ></b-table>
            <b-pagination
             v-model="currentPage"
-            :total-rows="employeeList.length"
+            :total-rows="records.length"
             :per-page="perPage"
             aria-controls="my-table"
             ></b-pagination>
-        <template >
-            <b-button size="lg" @click="exportToExcel()" >{{ _("wb_Export") }}</b-button>            
-        </template>
-        
         </iv-card>
         
     </div>
@@ -62,77 +59,27 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { RegisterRouter } from '@/../core/router';
-import { toEnumInterface } from '@/../core';
+import { BasicReport, BasicReportImpl } from './BasicReport';
+import { config } from 'rxjs';
 
 @Component
-export default class DemographicReport extends Vue {
-    currentPage:number=1;
-    options=[];
-    perPage:number=10;
-    employeeList:any[]=[];
-    selectedColumns=[];
-    isBusy:boolean=false;
-    members:any[]=[];
+export default class DemographicRecord extends BasicReportImpl  {
+    
+    
     async created(){
-        let resp: any=await this.$server.R("/acs/member",{ "paging.all": "true" });
-        this.members=resp.results;
-        this.getData();
+        this.tableTitle= this._('w_EmployeeList');        
+        await this.getData();
     }
-    isMounted:boolean=false;
-    doMounted(){
-        this.isMounted=true;
-        this.options=this.fields().map(x=>{return{value:x.key,text:x.label}});
-        this.selectedColumns = this.fields().map(x=>x.key);
-    }
-    filterColumn(){
-        console.log(this.selectedColumns);
-        console.log(this.options);
-    }
-  private getData(filter?:any) {        
-        this.isBusy=true;        
-        this.employeeList=[];         
-        
-        for(let item of this.members) {
-            let department=item.CustomFields&&item.CustomFields.length>0? item.CustomFields.find(x => x.FiledName=="CustomTextBoxControl5__CF_CF_CF"):undefined;
-            let departmentName=department&&department.FieldValue? department.FieldValue:'';
-            let costCenter=item.CustomFields&&item.CustomFields.length>0? item.CustomFields.find(x => x.FiledName=="CustomTextBoxControl5__CF_CF_CF_CF"):undefined;
-            let costCenterName=costCenter&&costCenter.FieldValue? costCenter.FieldValue:'';
-            let workArea=item.CustomFields&&item.CustomFields.length>0? item.CustomFields.find(x => x.FiledName=="CustomTextBoxControl5__CF_CF_CF_CF_CF_CF"):undefined;
-            let workAreaName=workArea&&workArea.FieldValue? workArea.FieldValue:'';
-            if(item.AccessRules&&item.AccessRules.length>0){
-                for(let accessRule of item.AccessRules){
-                    this.employeeList.push(this.createEmployee(item,departmentName,costCenterName,workAreaName,accessRule.ObjectName));
-                }
-            }else{
-                this.employeeList.push(this.createEmployee(item,departmentName,costCenterName,workAreaName, ""));
-            }      
-        }
+    
+    
+  private async getData(filter?:any) {        
+        this.isBusy=true;            
+        let resp: any=await this.$server.R("/report/memberrecord" as any, filter);
+        this.data = resp.results;
+        this.records = resp.results;
         this.isBusy=false;
-        if(!filter)return;
-        if(filter.LastName) this.employeeList= this.employeeList.filter(x=>x.LastName.toLowerCase().indexOf(filter.LastName.toLowerCase())>-1);
-        if(filter.FirstName) this.employeeList= this.employeeList.filter(x=>x.FirstName.toLowerCase().indexOf(filter.FirstName.toLowerCase())>-1);
-        if(filter.EmployeeNumber) this.employeeList= this.employeeList.filter(x=>x.EmployeeNumber.toLowerCase().indexOf(filter.EmployeeNumber.toLowerCase())>-1);
-        if(filter.CardNumber) this.employeeList= this.employeeList.filter(x=>x.CardNumber.toLowerCase().indexOf(filter.CardNumber.toLowerCase())>-1);
-        if(filter.CostCenterName) this.employeeList= this.employeeList.filter(x=>x.CostCenterName.toLowerCase().indexOf(filter.CostCenterName.toLowerCase())>-1);
-        if(filter.WorkAreaName) this.employeeList= this.employeeList.filter(x=>x.WorkAreaName.toLowerCase().indexOf(filter.WorkAreaName.toLowerCase())>-1);
-        if(filter.PermissionList) this.employeeList= this.employeeList.filter(x=>x.PermissionList.toLowerCase().indexOf(filter.PermissionList.toLowerCase())>-1);
-        
-        
-        
   }
 
-  private createEmployee(item: any,departmentName: string,costCenterName: string,workAreaName: string, accessRule: string) {
-    return {
-      FirstName: item.FirstName,
-      LastName: item.LastName,
-      EmployeeNumber: item.EmployeeNumber,
-      CardNumber: item.Credentials&&item.Credentials.length>0? item.Credentials.map(x => x.CardNumber).join(", "):"",
-      DepartmentName: departmentName,
-      CostCenterName: costCenterName,
-      WorkAreaName: workAreaName,
-      PermissionList: accessRule
-    }
-  }
 
     inf(){
         return `interface {
@@ -174,8 +121,10 @@ export default class DemographicReport extends Vue {
             }`;
             
     }
-    fields(){
-        return [            
+    isMounted:boolean=false;
+    doMounted(){
+        this.fields = 
+        [            
             {
                 key:"FirstName",
                 label: this._('w_Member_ChineseName1'),
@@ -211,9 +160,18 @@ export default class DemographicReport extends Vue {
                 label: this._("w_Permission_PermissionList")
             }
         ];
+        this.isMounted=true;
+        this.options=this.fields.map(x=>{return{value:x.key,text:x.label}});
+        this.selectedColumns = this.fields.map(x=>x.key);
+        console.log(this.options.length);
     }
     onSubmit($events){        
         this.getData($events);
     }
 }
 </script>
+<style lang="scss" scoped>
+.btn-filter{
+    margin: 0 10px;
+}
+</style>
