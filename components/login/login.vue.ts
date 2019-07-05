c9890d61038f1aa6fb7441a4b18048bf6dc17a32
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-07-05 10:11:34 +0800

    Finish menu permission and login redirection

--- components/login/login.vue.ts
+++ components/login/login.vue.ts
@@ -2,7 +2,7 @@ import { Vue, Component } from "vue-property-decorator";
 import { RegisterLoginRouter } from '@/../core';
 import { ServerName, ServerVersion } from '@/../core/server';
 import { ModalResponse } from '@/../components/modal';
-import { PermisisonList} from '@/../src/constants/permissions.ts';
+
 @RegisterLoginRouter({
     name: "_('wb_Login')",
     path: "/login",
@@ -14,22 +14,13 @@ export default class Login extends Vue {
     private version = `${ServerName} ${ServerVersion}`;
     private username: string = '';
     private password: string = '';
-
+        
     async Login() {
         let result = await this.$login({
             username: this.username,
             password: this.password
         });
-        if(!result.permissions || result.permissions.length<=0)this.$router.push("/");
-
-        for(let perm of result.permissions.filter(x=>x.access.R===true)){            
-            let getRouter = PermisisonList.find(x=>x.key == perm.of.identifier);
-            if(!getRouter.route)continue;
-            this.$router.push(getRouter.route);
-            break;    
-        }
-        
-        
+        this.$router.push("/redirector");
         
     }
 }
