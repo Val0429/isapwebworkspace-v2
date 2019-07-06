22900b6b57afea350d91ae7e86b9bbf6bc4a1fc2
Tina.Lin <7070587@gmail.com>
2019-07-06 15:35:12 +0800

    OccupancyDetailsTable  with fake data

--- components/Reports/OccupancyDetailsTable.vue
+++ components/Reports/OccupancyDetailsTable.vue
@@ -28,10 +28,10 @@
                     <td>{{ value.time }}</td>
                     <td>{{ value.numberDetected }}</td>
                     <td>
-                            <img
-                                class="threshold-image"
-                                :src="src"
-                            ><br>
+                        <img
+                            class="threshold-image"
+                            :src="value.src"
+                        ><br>
                     </td>
                 </tr>
                 </tbody>
@@ -40,8 +40,7 @@
                 <b-button
                     variant="secondary"
                     size="lg"
-                    @click="modalShow = !modalShow"
-                >{{ _('w_BackToList') }}
+                >{{ _('w_Back') }}
                 </b-button>
             </template>
 
@@ -54,6 +53,7 @@
     import { Vue, Component, Prop, Emit, Model, Watch } from "vue-property-decorator";
     import { toEnumInterface } from "@/../core";
     import ResponseFilter from "@/services/ResponseFilter";
+    import Datetime from '@/services/Datetime';
 
     @Component({
         components: {}
@@ -62,12 +62,13 @@
 
         // Prop
         // @Prop({
-        //     type: Boolean, // Boolean, Number, String, Array, Object
-        //     default: false
+        //     type: Array, // Boolean, Number, String, Array, Object
+        //     default: () => []
         // })
-        // modalShow: boolean;
+        // thresholdDetailTableContent: object;
 
         thresholdDetailTableTitle: any = [];
+        thresholdDetailTableContent: any = [];
 
 
         created() {
@@ -82,6 +83,22 @@
                 this._("w_Occupancy_NumberDetected"),
                 this._("w_Occupancy_Snapshot"),
             ];
+
+            // 假資料
+
+            for (let i = 0; i < 10 ; i++) {
+                let detailObject = {
+                    time: Datetime.DateTime2String(new Date(), "YYYY/MM/DD HH:mm"),
+                    numberDetected: i % 3 ? 5 : 7,
+                    src: i % 3
+                        ? 'https://scontent-sea1-1.cdninstagram.com/vp/12efb734c004cb99ab09bff758369aeb/5DAEB837/t51.2885-15/e35/61136397_818365191881804_7621382242543634635_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com&se=7&ig_cache_key=MjA0OTAzOTgxNTkwMjU5NjA4NQ%3D%3D.2'
+                        : 'https://pbs.twimg.com/media/Dj1lCA5U8AANFmY.jpg',
+                };
+                this.thresholdDetailTableContent.push(detailObject)
+            }
+
+
+
         }
 
         doCancel() {
@@ -95,6 +112,7 @@
 
 <style lang="scss" scoped>
     .threshold-image {
+        max-width: 300px;
         margin-bottom: 5px;
     }
 
