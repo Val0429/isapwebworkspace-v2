816cb02aaa4fc9f03cdf5f12e6197c0f291d71de
Morris's Macbook Pro <Morris.Hsu@isapsolution.com>
2019-07-12 15:11:54 +0800

    console is object in Report Highcharts

--- components/Reports/HighchartsDemographic.vue
+++ components/Reports/HighchartsDemographic.vue
@@ -354,7 +354,7 @@ export class HighchartsDemographic extends Vue {
             this.timeMode,
             this.areaMode,
             this.sites,
-            JSON.stringify(this.value)
+            this.value
         );
 
         // TODO: For Demo
