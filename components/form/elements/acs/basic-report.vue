f1b6608a3a76d29165998926198045486a7a66e9
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-07-15 20:47:17 +0800

    Enlarge report field selection

--- components/form/elements/acs/basic-report.vue
+++ components/form/elements/acs/basic-report.vue
@@ -19,7 +19,7 @@
         <iv-sort-select
                 v-if="options.length>0"                      
                 v-model="selectedColumns"
-                class="col-md-6"
+                class="col-md-8"
                 v-on:input="onOptionsChange($event)"
                 :options="options"
             />
