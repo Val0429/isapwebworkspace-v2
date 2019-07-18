ea14e1bffa73949988597cbe6860dd83fbd2fd97
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-07-18 10:27:48 +0800

    Change global error handler to production only

--- main.ts
+++ main.ts
@@ -2,18 +2,19 @@ import { Vue, Language } from "@/../core";
 import Dialog from "@/services/Dialog/Dialog";
 import debug from '@/config/default/debug';
 
-Vue.config.errorHandler = (error, vm, info) => {
-    let lang = new Language();
-    console.error("Vue.config.errorHandler", vm.$route, error);
-    let err :any = error as any;
-    if(err.res && err.res.statusCode && err.res.statusCode===401 && vm.$route.fullPath!=="/login") {        
-        console.error("redirected to login");
-        alert(lang.translate("w_RedirectToLoginMessage"));
-        vm.$router.push("/login");
-    }
-    
-}
+
 if(debug.prodMode){
+    Vue.config.errorHandler = (error, vm, info) => {
+        let lang = new Language();
+        console.error("Vue.config.errorHandler", vm.$route, error);
+        let err :any = error as any;
+        if(err.res && err.res.statusCode && err.res.statusCode===401 && vm.$route.fullPath!=="/login") {        
+            console.error("redirected to login");
+            alert(lang.translate("w_RedirectToLoginMessage"));
+            vm.$router.push("/login");
+        }
+        
+    }
     //remove console log
     window.console.log = function(){}
 }
