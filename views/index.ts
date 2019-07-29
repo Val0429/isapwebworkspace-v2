d65d3342509c5fdd5e9aebd20e9be9c019b38019
Morris's Macbook Pro <Morris.Hsu@isapsolution.com>
2019-07-29 10:35:38 +0800

    rule engine page

--- views/index.ts
+++ views/index.ts
@@ -71,7 +71,11 @@ import FaceRecognition from './VideoSource/FaceRecognition.vue';
 import Heatmap from './VideoSource/Heatmap.vue';
 import HumanDetection from './VideoSource/HumanDetection.vue';
 
-// Rules and Action
+// Rule Engine
+import RuleEngineTraffic from './RuleEngine/RuleEngineTraffic.vue';
+import RuleEngineDemographic from './RuleEngine/RuleEngineDemographic.vue';
+import RuleEngineHeatmap from './RuleEngine/RuleEngineHeatmap.vue';
+import RuleEngineRepeatVisitor from './RuleEngine/RuleEnigneRepeatVisitor.vue';
 
 // BusinessOperation
 import CampaignSetting from './BusinessOperation/CampaignSetting.vue';
@@ -162,16 +166,20 @@ $({ path: '/server/demographic_server', name: "_('w_Navigation_Server_Demographi
 $({ path: '/server/hd_server', name: "_('w_Navigation_Server_HDServer')", icon: 'fa-check' })(HumanDetectionServer);
 
 // Video Source (c)
-$({ path: '/video_sources', name: 'VideoSources', icon: 'fa-code', redirect: '/login' })();
-$({ path: '/video_sources/people_counting', name: 'VideoSources_PeopleCounting', icon: 'fa-check' })(PeopleCounting);
-$({ path: '/video_sources/demographic', name: 'VideoSources_Demographic', icon: 'fa-check' })(Demographic);
-$({ path: '/video_sources/dwell_time', name: 'VideoSources_DwellTime', icon: 'fa-check' })(DwellTime);
-$({ path: '/video_sources/human_detection', name: 'VideoSources_HumanDetection', icon: 'fa-check' })(HumanDetection);
-$({ path: '/video_sources/heatmap', name: 'VideoSources_Heatmap', icon: 'fa-check' })(Heatmap);
-$({ path: '/video_sources/face_recognition', name: 'VideoSources_FaceRecognition', icon: 'fa-code' })(FaceRecognition);
+$({ path: '/video_sources', name: "_('w_Navigation_VideoSources')", icon: 'fa-code', redirect: '/login' })();
+$({ path: '/video_sources/people_counting', name: "_('w_Navigation_VideoSources_PeopleCounting')", icon: 'fa-check' })(PeopleCounting);
+$({ path: '/video_sources/demographic', name: "_('w_Navigation_VideoSources_Demographic')", icon: 'fa-check' })(Demographic);
+$({ path: '/video_sources/dwell_time', name: "_('w_Navigation_VideoSources_DwellTime')", icon: 'fa-check' })(DwellTime);
+$({ path: '/video_sources/human_detection', name: "_('w_Navigation_VideoSources_HumanDetection')", icon: 'fa-check' })(HumanDetection);
+$({ path: '/video_sources/heatmap', name: "_('w_Navigation_VideoSources_Heatmap')", icon: 'fa-check' })(Heatmap);
+$({ path: '/video_sources/face_recognition', name: "_('w_Navigation_VideoSources_FaceRecognition')", icon: 'fa-code' })(FaceRecognition);
 
-// Rules and Action (?)
-$({ path: '/rules_actions', name: "_('w_Navigation_RulesActions')", icon: 'fa-question', redirect: '/login' })();
+// Rule Engine
+$({ path: '/rule_engine', name: "_('w_Navigation_RuleEngine')", icon: 'fa-code', redirect: '/rule_engine/traffic' })();
+$({ path: '/rule_engine/traffic', name: "_('w_Navigation_RuleEngine')", icon: 'fa-code' })(RuleEngineTraffic);
+$({ path: '/rule_engine/demographic', name: "_('w_Navigation_RuleEngine')", icon: 'fa-code' })(RuleEngineDemographic);
+$({ path: '/rule_engine/heatmap', name: "_('w_Navigation_RuleEngine')", icon: 'fa-code' })(RuleEngineHeatmap);
+$({ path: '/rule_engine/repeat_visitor', name: "_('w_Navigation_RuleEngine')", icon: 'fa-code' })(RuleEngineRepeatVisitor);
 
 // Business Operation
 $({ path: '/business_operations', name: "_('w_Navigation_BusinessOperation')", redirect: '/business_operations/campaign_setting' })();
