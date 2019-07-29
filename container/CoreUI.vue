7216bc0b58df9f306a7e480d485497926916797a
Morris's Macbook Pro <Morris.Hsu@isapsolution.com>
2019-07-29 11:41:37 +0800

    remove alive

--- container/CoreUI.vue
+++ container/CoreUI.vue
@@ -299,72 +299,12 @@ export default class CoreUI extends Vue {
         }
     }
 
-    // WebSocket Alive
-    showLoadingSecond = 3000;
-    wsOpenStatus: boolean = false;
-    ws: Ws = new Ws({
-        url: "",
-        OnOpen: async (e: Event): Promise<void> => {
-            this.setWsOpenStatus(true);
-            console.log("WS Alive Open");
-        },
-        OnMessage: async (e: MessageEvent): Promise<void> => {
-            if (e.data.length > 0) {
-                this.handleWs(e.data);
-            }
-        },
-        OnError: async (e: Event): Promise<void> => {
-            this.setWsOpenStatus(false);
-            console.log("WS Alive Error");
-        },
-        OnClose: async (e: CloseEvent): Promise<void> => {
-            this.setWsOpenStatus(false);
-            console.log("WS Alive Colse");
-        }
-    });
-
     mounted() {
-        setTimeout(this.initWS, this.showLoadingSecond);
     }
 
     beforeDestroy() {
-        this.ws.Close();
     }
 
-    initWS() {
-        if (this.$user.sessionId != undefined) {
-            let url = `ws://${ServerConfig.host}:${ServerConfig.port}/server/alive?sessionId=${this.$user.sessionId}`;
-            this.ws.url = url;
-            this.ws.Connect();
-        }
-    }
-
-    setWsOpenStatus(value: boolean) {
-        this.wsOpenStatus = value;
-        if (value) {
-            Loading.hide();
-        } else {
-            setTimeout(this.setLoading, this.showLoadingSecond);
-        }
-    }
-
-    setLoading() {
-        if (!this.wsOpenStatus) {
-            Loading.show();
-        }
-    }
-
-    handleWs(wsData: string) {
-        Loading.hide();
-        try {
-            let data: any = JSON.parse(wsData);
-            if (data.statusCode != undefined && data.statusCode == 401) {
-                this.Logout();
-            }
-        } catch (e) {
-            console.log("WS handle error: ", e);
-        }
-    }
 }
 </script>
 
