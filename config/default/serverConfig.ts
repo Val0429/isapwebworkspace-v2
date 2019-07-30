714d6141b5574f35753e62f6301e50ad7644bf40
Val Liu <val.liu@isapsolution.com>
2019-07-30 11:48:24 +0800

    update config

--- config/default/serverConfig.ts
+++ config/default/serverConfig.ts
@@ -1,3 +1,11 @@
+/*
+ * Created on Tue Jul 30 2019
+ * Author: Val Liu
+ * Copyright (c) 2019, iSAP Solution
+ */
+
+import { Config } from '@/../config/default/serverConfig';
+
 var config: Config = {
     ip: "localhost",
     port: 6060,
@@ -6,14 +14,3 @@ var config: Config = {
     serverFrameworkVersionGreaterThan: "1.06.00"
 }
 export default config;
-
-export interface Config {
-    /// for debug only. normally client & server share different port
-    /// if without specific ip / port / ssl, will use current browser url
-    ip?: string;
-    port?: number;
-    ssl?: boolean;
-
-    /// only allow server framework > THIS to do login
-    serverFrameworkVersionGreaterThan: string;
-}
