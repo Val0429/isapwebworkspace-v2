2d4772134634c965109845eef88329325352646e
Tina.Lin <7070587@gmail.com>
2019-08-02 10:24:37 +0800

    add components step

---
+++ components/ContractorRegistration/Step1.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        Components Empty
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Emit, Model } from "vue-property-decorator";
+
+@Component({
+    components: {}
+})
+export class Step1 extends Vue {
+    // Prop
+    @Prop({
+        type: String, // Boolean, Number, String, Array, Object
+        default: ""
+    })
+    label: string;
+
+    // Model
+    @Model("model", {
+        type: String,
+        default: ""
+    })
+    value: string;
+
+    inputData = "Test input data";
+    modelData = "";
+
+    created() {
+        this.modelData = this.value;
+    }
+
+    mounted() {
+        this.start();
+    }
+
+    start() {
+        this.$emit("input", this.inputData);
+    }
+
+    putModel() {
+        this.$emit("model", this.modelData);
+    }
+}
+
+export default Step1;
+Vue.component("step1", Step1);
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ components/ContractorRegistration/Step8.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        Components Empty
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Emit, Model } from "vue-property-decorator";
+
+@Component({
+    components: {}
+})
+export class Step8 extends Vue {
+    // Prop
+    @Prop({
+        type: String, // Boolean, Number, String, Array, Object
+        default: ""
+    })
+    label: string;
+
+    // Model
+    @Model("model", {
+        type: String,
+        default: ""
+    })
+    value: string;
+
+    inputData = "Test input data";
+    modelData = "";
+
+    created() {
+        this.modelData = this.value;
+    }
+
+    mounted() {
+        this.start();
+    }
+
+    start() {
+        this.$emit("input", this.inputData);
+    }
+
+    putModel() {
+        this.$emit("model", this.modelData);
+    }
+}
+
+export default Step8;
+Vue.component("step8", Step8);
+</script>
+
+<style lang="scss" scoped>
+</style>
