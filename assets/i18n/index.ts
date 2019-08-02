dc82af8e746af763a695f2fd3bff651ef275bb01
Morris's Macbook Pro <Morris.Hsu@isapsolution.com>
2019-08-02 10:22:58 +0800

    Navigation

--- assets/i18n/index.ts
+++ assets/i18n/index.ts
@@ -1,3 +1,1 @@
 export * from './en-US';
-export * from './zh-TW';
-export * from './ja-JP';
---
+++ services/Transition/ETransition.ts
@@ -0,0 +1,7 @@
+enum ETransitionType {
+    none = 'none',
+    directionSlide = 'iv-direction-slide',
+    fadeSlide = 'iv-fade-slide',
+}
+
+export { ETransitionType };
