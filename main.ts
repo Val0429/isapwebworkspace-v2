f810970f7ecdb4bea27d95e3887fd6a4a18578c6
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-08-07 16:43:19 +0800

    Fix global error handler following framework changes

--- main.ts
+++ main.ts
@@ -3,18 +3,28 @@ import Dialog from "@/services/Dialog/Dialog";
 import debug from '@/config/default/debug';
 
 
-if(debug.prodMode){
-    Vue.config.errorHandler = (error, vm, info) => {
+//if(debug.prodMode){
+    Vue.config.errorHandler = async (error, vm, info) => {
         let lang = new Language();
         console.error("Vue.config.errorHandler", vm.$route, error);
         let err :any = error as any;
         if(err.res && err.res.statusCode && err.res.statusCode===401 && vm.$route.fullPath!=="/login") {        
             console.error("redirected to login");
-            alert(lang.translate("w_RedirectToLoginMessage"));
-            vm.$router.push("/login");
+            await Dialog.confirm(
+                lang.translate("w_RedirectToLoginMessage"),
+                lang.translate("w_Error"),
+                () => {
+                    vm.$router.push("/login");
+                }
+              );
+            
+        }
+        if(err.res && err.res.statusCode && err.res.statusCode===400 && err.body ) { 
+            await Dialog.error(err.body);            
         }
         
     }
     //remove console log
+if(debug.prodMode){
     window.console.log = function(){}
 }
