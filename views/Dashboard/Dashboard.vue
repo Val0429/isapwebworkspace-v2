079dce7140e418a1f02931e4575ff47b3396c3e8
Morris's Macbook Pro <Morris.Hsu@isapsolution.com>
2019-08-12 19:37:35 +0800

    dashboard api over, but not check

--- views/Dashboard/Dashboard.vue
+++ views/Dashboard/Dashboard.vue
@@ -88,7 +88,7 @@ import { ITransition } from "@/services/Transition";
 import Dialog from "@/services/Dialog";
 import Datetime from "@/services/Datetime";
 import { chart } from "highcharts";
-import ResponseFilter from '@/services/ResponseFilter';
+import ResponseFilter from "@/services/ResponseFilter";
 
 enum EChartMode {
     chartByHours,
@@ -124,12 +124,22 @@ export default class Dashboard extends Vue {
 
     visible: boolean = true;
     EChartMode = EChartMode;
-    startTimeByDays = new Date("1990-1-1 00:00");
-    endTimeByDays = new Date("1990-1-2 00:00");
-    startTimeByHours = new Date("1990-1-1 00:00");
-    endTimeByHours = new Date("1990-1-2 00:00");
-    chartDataByHours: ISeries[] = [];
-    chartDataByDays: ISeries[] = [];
+    startTimeByDays: Date = new Date();
+    endTimeByDays: Date = new Date();
+    startTimeByHours: Date = new Date();
+    endTimeByHours: Date = new Date();
+    chartDataByHours: ISeries[] = [
+        {
+            name: "all",
+            data: []
+        }
+    ];
+    chartDataByDays: ISeries[] = [
+        {
+            name: null,
+            data: [0, 0]
+        }
+    ];
     dashboardData: IDashboardData = {
         submitted: 0,
         approved: 0,
@@ -137,83 +147,144 @@ export default class Dashboard extends Vue {
         total: 0
     };
 
-    created() {}
+    dateEqualFormat = "YYYY-MM-DD";
+
+    created() {
+        this.initChartDate();
+    }
 
     mounted() {}
 
-    initdata() {
-        //false data
-        this.dashboardData = {
-            submitted: 7,
-            approved: 15,
-            awaitingApproval: 70,
-            total: 93
-        };
+    initChartDate() {
+        this.inputFilterData.startDate = Datetime.MonthStart(new Date());
+        this.inputFilterData.endDate = Datetime.MonthEnd(new Date());
 
-        this.startTimeByHours = new Date("2019-8-5 00:00");
-        this.endTimeByHours = new Date("2019-8-6 00:00");
-        this.chartDataByHours = [
-            {
-                name: "all",
-                data: [
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    12.4,
-                    23.2,
-                    24.5,
-                    39.7,
-                    10,
-                    45.5,
-                    57.4,
-                    40.4,
-                    27.6,
-                    29.1,
-                    1.8,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0
-                ]
-            }
-        ];
+        let dayStart = Datetime.DateStart(new Date());
+        let dayEnd = Datetime.DateEnd(new Date());
+        dayStart.setDate(-2);
+        dayEnd.setDate(-1);
 
-        this.startTimeByDays = new Date("2019-8-5 00:00");
-        this.endTimeByDays = new Date("2019-8-6 00:00");
-        this.chartDataByDays = [
-            {
-                name: "Visitor",
-                data: [42.4, 99.1]
-            },
-            {
-                name: "Unregistered",
-                data: [0, 0]
-            }
-        ];
+        this.startTimeByHours = Datetime.DateStart(new Date());
+        this.endTimeByHours = Datetime.DateEnd(new Date());
+        this.startTimeByDays = dayStart;
+        this.endTimeByDays = dayEnd;
     }
 
     updateFilter(datas: any) {
         this.inputFilterData[datas.key] = datas.value;
-        console.log(this.inputFilterData);
     }
 
     async doSubmit() {
-        // TODO waiting for api
-        this.initdata();
+        // clear data
         this.visible = false;
+        this.chartDataByDays[0].data[0] = 0;
+        this.chartDataByDays[0].data[1] = 0;
+        this.chartDataByHours[0].data = [];
+
+        // Dashboard API
+        let param = {
+            startDate: this.inputFilterData.startDate.toISOString(),
+            endDate: this.inputFilterData.endDate.toISOString()
+        };
 
         await this.$server
-            .C("/flow1/crms/dashboard")
+            .C("/flow1/crms/dashboard", param)
             .then((response: any) => {
-                console.log('response ~ ', response)
                 ResponseFilter.successCheck(this, response, (response: any) => {
+                    // Left Top
+                    if (response.leftTop != undefined) {
+                        let totalLeftTop = 0;
+                        if (response.leftTop.approve != undefined) {
+                            this.dashboardData.approved =
+                                response.leftTop.approve;
+                            totalLeftTop += response.leftTop.approve;
+                        }
+                        if (response.leftTop.new != undefined) {
+                            this.dashboardData.submitted = response.leftTop.new;
+                            totalLeftTop += response.leftTop.new;
+                        }
+                        if (response.leftTop.pendding != undefined) {
+                            this.dashboardData.awaitingApproval =
+                                response.leftTop.pendding;
+                            totalLeftTop += response.leftTop.pendding;
+                        }
+                        this.dashboardData.total = totalLeftTop;
+                    }
 
+                    // Right Top
+                    let rightTopDatas = [];
+                    if (response.rightTop != undefined) {
+                        for (let i = 0; i < 24; i++) {
+                            let haveData = false;
+                            for (let loopData of response.rightTop) {
+                                if (
+                                    loopData.date != undefined &&
+                                    loopData.count != undefined &&
+                                    typeof loopData.count == "number"
+                                ) {
+                                    let tempDate: Date = loopData.date as Date;
+                                    if (tempDate.getHours() == i) {
+                                        haveData = true;
+                                        rightTopDatas.push(loopData.count);
+                                        break;
+                                    }
+                                }
+                            }
+                            if (!haveData) {
+                                rightTopDatas.push(0);
+                            }
+                        }
+                    } else {
+                        for (let i = 0; i < 24; i++) {
+                            rightTopDatas.push(0);
+                        }
+                    }
+                    this.chartDataByHours[0].data = rightTopDatas;
+
+                    // Bottom
+                    if (response.bottom != undefined) {
+                        for (let loopData of response.bottom) {
+                            if (loopData.date == undefined) {
+                                continue;
+                            }
+
+                            if (loopData.count == undefined) {
+                                continue;
+                            }
+
+                            if (typeof loopData.count != "number") {
+                                continue;
+                            }
+
+                            if (
+                                Datetime.DateTime2String(
+                                    loopData.date,
+                                    this.dateEqualFormat
+                                ) ==
+                                Datetime.DateTime2String(
+                                    this.startTimeByDays,
+                                    this.dateEqualFormat
+                                )
+                            ) {
+                                this.chartDataByDays[0].data[0] +=
+                                    loopData.count;
+                            }
+
+                            if (
+                                Datetime.DateTime2String(
+                                    loopData.date,
+                                    this.dateEqualFormat
+                                ) ==
+                                Datetime.DateTime2String(
+                                    this.endTimeByDays,
+                                    this.dateEqualFormat
+                                )
+                            ) {
+                                this.chartDataByDays[0].data[1] +=
+                                    loopData.count;
+                            }
+                        }
+                    }
                 });
             })
             .catch((e: any) => {
@@ -224,8 +295,6 @@ export default class Dashboard extends Vue {
     IFilterConditionForm() {
         return `
             interface {
-
-
                 /**
                  * @uiLabel - ${this._("w_Startdate")}
                  * @uiColumnGroup - timeGroup
