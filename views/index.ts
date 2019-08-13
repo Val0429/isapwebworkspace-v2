5c086870aed6c3ee46c5f321f554afd3dd9dd19b
Morris's Macbook Pro <Morris.Hsu@isapsolution.com>
2019-08-13 14:27:14 +0800

    update icon

--- views/index.ts
+++ views/index.ts
@@ -19,9 +19,9 @@ $({ path: '/my_profile', name: "_('w_Navigation_MyProfile')", icon: 'fa-check' }
 import ReportsDashboard from './Reports/ReportsDashboard.vue';
 import ReportsInversigation from './Reports/ReportsInversigation.vue';
 
-$({ path: '/reports', name: "_('w_Navigation_Reports')", icon: 'fa-check', redirect: '/reports/dashboard' })();
-$({ path: '/reports/dashboard', name: "_('w_Navigation_Reports_Dashboard')", icon: 'fa-check' })(ReportsDashboard);
-$({ path: '/reports/investigation', name: "_('w_Navigation_Reports_Investigation')", icon: 'fa-check' })(ReportsInversigation);
+$({ path: '/reports', name: "_('w_Navigation_Reports')", icon: 'fa-columns', redirect: '/reports/dashboard' })();
+$({ path: '/reports/dashboard', name: "_('w_Navigation_Reports_Dashboard')", icon: 'fa-tachometer' })(ReportsDashboard);
+$({ path: '/reports/investigation', name: "_('w_Navigation_Reports_Investigation')", icon: 'fa-eye' })(ReportsInversigation);
 
 // Setups
 import SetupsAccount from './Setups/SetupsAccount.vue';
@@ -35,17 +35,17 @@ import SetupsSMS from './Setups/SetupsSMS.vue';
 import SetupsSMSOnline from './Setups/SetupsSMSOnline.vue';
 import SetupsVisitorCard from './Setups/SetupsVisitorCard.vue';
 
-$({ path: '/setups', name: "_('w_Navigation_Setups')", icon: 'fa-check', redirect: '/setups/account' })();
-$({ path: '/setups/floor', name: "_('w_Navigation_Setups_Floor')", icon: 'fa-check' })(SetupsFloor);
-$({ path: '/setups/company', name: "_('w_Navigation_Setups_Company')", icon: 'fa-check' })(SetupsCompany);
-$({ path: '/setups/account', name: "_('w_Navigation_Setups_Account')", icon: 'fa-check' })(SetupsAccount);
-$({ path: '/setups/kiosk', name: "_('w_Navigation_Setups_Kiosk')", icon: 'fa-check' })(SetupsKiosk);
-$({ path: '/setups/license', name: "_('w_Navigation_Setups_License')", icon: 'fa-check' })(SetupsLicense);
-$({ path: '/setups/frs', name: "_('w_Navigation_Setups_FRS')", icon: 'fa-check' })(SetupsFRS);
-$({ path: '/setups/email', name: "_('w_Navigation_Setups_Email')", icon: 'fa-check' })(SetupsEmail);
-$({ path: '/setups/sms', name: "_('w_Navigation_Setups_SMS')", icon: 'fa-check' })(SetupsSMS);
-$({ path: '/setups/sms_online', name: "_('w_Navigation_Setups_SMSOnline')", icon: 'fa-check' })(SetupsSMSOnline);
-$({ path: '/setups/visitor_card', name: "_('w_Navigation_Setups_VisitorCard')", icon: 'fa-check' })(SetupsVisitorCard);
+$({ path: '/setups', name: "_('w_Navigation_Setups')", icon: 'fa-cog', redirect: '/setups/account' })();
+$({ path: '/setups/floor', name: "_('w_Navigation_Setups_Floor')", icon: 'fa-building' })(SetupsFloor);
+$({ path: '/setups/company', name: "_('w_Navigation_Setups_Company')", icon: 'fa-industry' })(SetupsCompany);
+$({ path: '/setups/account', name: "_('w_Navigation_Setups_Account')", icon: 'fa-user' })(SetupsAccount);
+$({ path: '/setups/kiosk', name: "_('w_Navigation_Setups_Kiosk')", icon: 'fa-barcode' })(SetupsKiosk);
+$({ path: '/setups/license', name: "_('w_Navigation_Setups_License')", icon: 'fa-certificate' })(SetupsLicense);
+$({ path: '/setups/frs', name: "_('w_Navigation_Setups_FRS')", icon: 'fa-server' })(SetupsFRS);
+$({ path: '/setups/email', name: "_('w_Navigation_Setups_Email')", icon: 'fa-inbox' })(SetupsEmail);
+$({ path: '/setups/sms', name: "_('w_Navigation_Setups_SMS')", icon: 'fa-envelope' })(SetupsSMS);
+$({ path: '/setups/sms_online', name: "_('w_Navigation_Setups_SMSOnline')", icon: 'fa-cloud' })(SetupsSMSOnline);
+$({ path: '/setups/visitor_card', name: "_('w_Navigation_Setups_VisitorCard')", icon: 'fa-id-card' })(SetupsVisitorCard);
 
 // Tenants
 import TenantsInvitation from './Tenants/TenantsInvitation.vue';
