1f73b1149503c2b3c151fbc3da63ff0025abdadb
Tina.Lin <7070587@gmail.com>
2019-08-16 09:33:59 +0800

    update number regex

--- services/RegexServices/index.ts
+++ services/RegexServices/index.ts
@@ -3,7 +3,7 @@ export class RegexService {
 
     regexItem: any = {
         url: /(ws|ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/,
-        number: /[^\d]/g,
+        number: /^[0-9]*$/,
     };
 
     url(data: string): boolean {
