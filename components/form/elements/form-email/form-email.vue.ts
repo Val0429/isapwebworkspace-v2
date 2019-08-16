86b1269466ae9ba6322c3b05e9d2c349ea26abb4
benlee0918 <bingxain@hotmail.com>
2019-08-16 10:26:37 +0800

    fix doashbaod  Pending Approval size and add form-email

---
+++ components/form/elements/form-email/form-email.vue.ts
@@ -0,0 +1,59 @@
+/*
+ * Created on Tue Jul 30 2019
+ * Author: Val Liu
+ * Copyright (c) 2019, iSAP Solution
+ */
+
+import { Vue, Component, Prop, Model } from 'vue-property-decorator';
+
+const emailfilter = {
+    bind: (el: any, a, b) => {
+        el = el.getElementsByTagName('input')[0];
+        let oldvalue = el.value;
+        el.addEventListener('input', (evt) => {
+            let newvalue = el.value;
+            let regex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
+            main: do {
+                if (!regex.test(newvalue)) break;
+                // let arytmp = newvalue.split('.');
+                // for (let part of arytmp) {
+                //     if (part.length === 0) continue;
+                //     let num = +part;
+                //     if (num < 0 || num > 255) break main;
+                // }
+
+                // oldvalue = newvalue;
+                return;
+            } while (0);
+
+            let { selectionStart, selectionEnd } = el;
+            el.value = oldvalue;
+            el.selectionStart = selectionStart - 1;
+            el.selectionEnd = selectionEnd - 1;
+        });
+    },
+};
+
+@Component({
+    directives: { emailfilter },
+})
+export class FormEmail extends Vue {
+    @Prop({
+        type: String,
+        required: false,
+    })
+    label!: string;
+
+    @Model('input', {
+        type: String,
+        required: false,
+    })
+    value!: string;
+
+    @Prop({ type: String, required: false })
+    placeholder!: string;
+
+    @Prop({ type: String, required: false })
+    invalid!: string;
+}
+export default FormEmail;
