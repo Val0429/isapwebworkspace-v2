9d62e00ca27b66d775b417fa7b72496375c511bc
benlee0918 <bingxain@hotmail.com>
2019-08-20 17:46:36 +0800

    updae CurrentSatusDashboard

--- views/Dashboard/Overview.vue
+++ views/Dashboard/Overview.vue
@@ -5,13 +5,24 @@
             :step="transition.step"
             :type="transition.type"
         >
-
             <div
                 key="transition_1"
                 v-show="transition.step === 1"
                 :label="'Empty 1'"
+                class="row"
             >
-                <current-status-dashboard></current-status-dashboard>
+                <iv-card
+                    class="current-status-card"
+                    :label="_('w_DashboardOverview_CurrentStatus')"
+                >
+                    <current-status-dashboard></current-status-dashboard>
+                </iv-card>
+                <iv-card
+                    class="filter-status-card"
+                    :label="_('w_DashboardOverview_FilterStatus')"
+                >
+                    <current-status-dashboard></current-status-dashboard>
+                </iv-card>
             </div>
 
             <div
@@ -54,6 +65,14 @@ export default class Overview extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.current-status-card {
+    width: 23%;
+    margin: 1%;
+}
+.filter-status-card {
+    width: 73%;
+    margin: 1%;
+}
 </style>
 
 
