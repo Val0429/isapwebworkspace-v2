f92d269c7c62a56539e75cc0aada157ce43681c7
Morris's Macbook Pro <Morris.Hsu@isapsolution.com>
2019-08-20 11:31:00 +0800

    append date format for table row in components

--- views/components/Table.vue
+++ views/components/Table.vue
@@ -1,10 +1,12 @@
 <template>
     <div key="main">
-        <p><iv-language-button /></p>
+        <p>
+            <iv-language-button />
+        </p>
 
         <iv-table
             :server="{ path: '/test/me' }"
-            interface="
+            interface='
             interface {
                 /**
                  * @uiType - iv-cell-auto-index
@@ -17,18 +19,23 @@
                  * @uiType - iv-form-switch
                  */
                 testboolean: boolean;
+
+                /**
+                 * @uiType - iv-cell-date
+                 * @uiAttrs - { format: "YYYY/MM/DD HH:mm:ss dddd ddd MMMM MMM A a" }
+                 */
                 createdAt: Date;
                 updatedAt: Date;
             }
-            "
-            />
+            '
+        />
     </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import { RegisterRouter } from '@/../core/router';
-import { toEnumInterface } from '@/../core';
+import { Component, Vue } from "vue-property-decorator";
+import { RegisterRouter } from "@/../core/router";
+import { toEnumInterface } from "@/../core";
 
 @Component
 export default class Table extends Vue {
