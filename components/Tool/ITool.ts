0d2d700e26a4d7e6546f37d1c53b0d49ba873e7b
Tina.Lin <7070587@gmail.com>
2019-08-23 14:57:22 +0800

    temp save Traffic Filter Condition

--- components/Tool/ITool.ts
+++ components/Tool/ITool.ts
@@ -1,7 +1,25 @@
-interface IRepeatVisitorFilter {
-	siteIds: string[];
-	startDate: Date | string;
-	endDate: Date | string;
+interface IDate {
+	startDate?: Date | string;
+	endDate?: Date | string;
 }
 
-export { IRepeatVisitorFilter }
+interface IFilter extends IDate {
+	siteId: string;
+	areaId: string;
+	deviceGroupId: string;
+	deviceId: string;
+	isEmployee: boolean | string;
+	date?: Date;
+	hour?: string
+}
+
+interface IRepeatVisitorFilter extends IDate {
+	siteIds: string[];
+}
+
+interface ITrafficFilter extends IFilter {
+	isIn: boolean | string;
+}
+
+
+export { IRepeatVisitorFilter, ITrafficFilter, IFilter }
